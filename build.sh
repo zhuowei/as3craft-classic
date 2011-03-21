6be8ed8bb3520f60cde249941d881c53d6f4fe4d
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2011-03-20 23:08:46 -0700

    Added ability to log into Minecraft.net

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mxmlc -theme+=themefix.css -omit-trace-statements=false -compiler.debug=true -static-link-runtime-shared-libraries -locale=en_US -source-path=locale/{locale} -incremental=true -keep-generated-actionscript=true AS3Craft.mxml
+amxmlc -theme+=themefix.css -omit-trace-statements=false -compiler.debug=true -static-link-runtime-shared-libraries -locale=en_US -source-path=locale/{locale} -incremental=true -keep-generated-actionscript=true AS3Craft.mxml
